a97d6672e70cd1277202a57e3d6da9c6671bbdc5
vinuc49 <vinuchandregowda@gmail.com>
2023-04-06 21:07:47 +0000

    made changes in print statement in display rules

--- run.py
+++ run.py
@@ -33,9 +33,47 @@ def welcome():
     print("GET EXCITED!\nLet's play Hangman!")
 
 
+def display_rules():
+    """
+    Ask user if game rules are needed and display game rules as requested
+    """
+    print("\nBefore we start, would you like to see the "
+           "instructions or are you good to go?")
+    rules_on = input("Press y if yes, "
+                     "or press enter to play the game:\n")
+    if rules_on.lower() == "y":
+        rules_txt()
+        print("Are you ready to play?\n")
+        input("Press enter to start the game >> \n")
+    else:
+        pass
+
+
+def rules_txt():
+    """
+    Display game rules
+    """
+    print("\nHere are rules on how to play \n"
+          "1. Select a category\n"
+          "2. The amount of  displayed Underscores '_' hints\n"
+          "   to the length of the word.\n"
+          "3. You need to guess the word\n"
+          "   by entering one letter at a time.\n"
+          "4. If your answer is correct, the letter will be displayed\n"
+          "   instead of the underscore'_'.\n"
+          "5. If you guess all the letters and the word is complete,\n"
+          "   you win the game\n"
+          "6. If the incorrect letter is entered,\n"
+          "   the hangman image will progress.\n"
+          "   Space between the words is considered incorrect.\n"
+          "7. If the number of incorrect attempts reaches the limit\n"
+          "   and the hangman image completed the game ends.\n")
+
+
 def main():
     display_title()  # print title function
     welcome()  # display user input name and greetings
+    display_rules()  # display rules if user chooses
 
 
 main()
