c7a36e7c5c966225a96f6523a904daac028bf791
vinuc49 <vinuchandregowda@gmail.com>
2023-04-06 20:35:13 +0000

    Add display art

--- run.py
+++ run.py
@@ -4,3 +4,20 @@ All Imports
 import random
 import time
 
+
+def display_greeting():
+    """
+    Prompt user to input name and greetings
+    """
+    # Title ASCII ART https://patorjk.com/software/taag
+    print(" __      _____ _    ___ ___  __  __ ___   _____ ___ ")
+    print(" \ \    / / __| |  / __/ _ \|  \/  | __| |_   _/ _ \ ")
+    print("  \ \/\/ /| _|| |_| (_| (_) | |\/| | _|    | || (_) |")
+    print("   \_/\_/ |___|____\___\___/|_|  |_|___|   |_| \___/  \n")     
+    print(" _  _   _   _  _  ___ __  __   _   _  _    ___   _   __  __ ___ ")
+    print("| || | /_\ | \| |/ __|  \/  | /_\ | \| |  / __| /_\ |  \/  | __|")
+    print("| __ |/ _ \| .` | (_ | |\/| |/ _ \| .` | | (_ |/ _ \| |\/| | _| ")
+    print("|_||_/_/ \_\_|\_|\___|_|  |_/_/ \_\_|\_|  \___/_/ \_\_|  |_|___|\n")
+  
+
+display_greeting()
